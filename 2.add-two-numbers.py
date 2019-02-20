5175c5a5267301d19d91581e6d1febc1ffdfe142
Wangdeyi <744731059@qq.com>
2019-02-20 08:53:44 +0800

    solution 2

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -42,7 +42,8 @@ class Solution:
         while l1 and l2:
             value = (l1.val + l2.val + carry) % 10
             carry = (l1.val + l2.val + carry) // 10
-            node.next = ListNode(value)
+            l1.val = value
+            node.next = l1
             node = node.next
             l1 = l1.next
             l2 = l2.next
@@ -50,17 +51,20 @@ class Solution:
         while l1:
             value = (l1.val + carry) % 10
             carry = (l1.val + carry) // 10
-            node.next = ListNode(value)
+            l1.val = value
+            node.next = l1
             node = node.next
             l1 = l1.next
 
         while l2:
             value = (l2.val + carry) % 10
             carry = (l2.val + carry) // 10
-            node.next = ListNode(value)
+            l2.val = value
+            node.next = l2
             node = node.next
             l2 = l2.next
 
         if carry:
             node.next = ListNode(carry)
-        return head.next 
+
+        return head.next
