1b43ca837fcc1f5b7d4e743b70c31bc97938a7e1
Wangdeyi <744731059@qq.com>
2019-02-21 14:37:42 +0800

    solution 9

--- 7.reverse-integer.py
+++ 7.reverse-integer.py
@@ -51,8 +51,8 @@ class Solution:
         while x > 0:
             res = res * 10 + x % 10
             x = x // 10
+            if res > 2**31-1 or res < -2**31:
+                return 0
         if flag:
             res = -res
-        if res > 2**31-1 or res < -2**31:
-            return 0
         return res
