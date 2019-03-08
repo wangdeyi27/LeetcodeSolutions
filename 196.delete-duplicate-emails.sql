d983aeb012032074d73f688ea2a1e01610f145b7
Wangdeyi <744731059@qq.com>
2019-03-08 17:29:19 +0800

    solution 196

---
+++ 196.delete-duplicate-emails.sql
@@ -0,0 +1,49 @@
+--
+-- @lc app=leetcode id=196 lang=mysql
+--
+-- [196] Delete Duplicate Emails
+--
+-- https://leetcode.com/problems/delete-duplicate-emails/description/
+--
+-- database
+-- Easy (30.25%)
+-- Total Accepted:    63.7K
+-- Total Submissions: 206.6K
+-- Testcase Example:  '{"headers": {"Person": ["Id", "Email"]}, "rows": {"Person": [[1, "john@example.com"], [2, "bob@example.com"], [3, "john@example.com"]]}}'
+--
+-- Write a SQL query to delete all duplicate email entries in a table named
+-- Person, keeping only unique emails based on its smallest Id.
+-- 
+-- 
+-- +----+------------------+
+-- | Id | Email            |
+-- +----+------------------+
+-- | 1  | john@example.com |
+-- | 2  | bob@example.com  |
+-- | 3  | john@example.com |
+-- +----+------------------+
+-- Id is the primary key column for this table.
+-- 
+-- 
+-- For example, after running your query, the above Person table should have
+-- the following rows:
+-- 
+-- 
+-- +----+------------------+
+-- | Id | Email            |
+-- +----+------------------+
+-- | 1  | john@example.com |
+-- | 2  | bob@example.com  |
+-- +----+------------------+
+-- 
+-- 
+-- Note:
+-- 
+-- Your output is the whole Person table after executing your sql. Use delete
+-- statement.
+-- 
+--
+# Write your MySQL query statement below
+Delete p1 from
+Person p1, Person p2
+where p1.Email=p2.Email and p1.Id>p2.Id
