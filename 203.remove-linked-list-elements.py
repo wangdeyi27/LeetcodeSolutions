dda8cc496d0c130c8db228b61430c62ef9be9129
Wangdeyi <744731059@qq.com>
2019-03-11 15:32:42 +0800

    solution 203

---
+++ 203.remove-linked-list-elements.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=203 lang=python3
+#
+# [203] Remove Linked List Elements
+#
+# https://leetcode.com/problems/remove-linked-list-elements/description/
+#
+# algorithms
+# Easy (35.12%)
+# Total Accepted:    208.8K
+# Total Submissions: 591.4K
+# Testcase Example:  '[1,2,6,3,4,5,6]\n6'
+#
+# Remove all elements from a linked list of integers that have value val.
+# 
+# Example:
+# 
+# 
+# Input:  1->2->6->3->4->5->6, val = 6
+# Output: 1->2->3->4->5
+# 
+# 
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        while head and head.val == val:
+            head = head.next
+        node = head
+        while node and node.next:
+            if node.next.val == val:
+                tmp = node.next.next
+                while tmp and tmp.val == val:
+                    tmp = tmp.next
+                node.next = tmp
+            node = node.next
+        return head
