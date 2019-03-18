75fb4e3b2738cd98d86675204227e68c508d8e27
Wangdeyi <744731059@qq.com>
2019-03-18 16:06:44 +0800

    solution 6

---
+++ 6.zigzag-conversion.py
@@ -0,0 +1,67 @@
+#
+# @lc app=leetcode id=6 lang=python3
+#
+# [6] ZigZag Conversion
+#
+# https://leetcode.com/problems/zigzag-conversion/description/
+#
+# algorithms
+# Medium (30.41%)
+# Total Accepted:    294K
+# Total Submissions: 951.1K
+# Testcase Example:  '"PAYPALISHIRING"\n3'
+#
+# The string "PAYPALISHIRING" is written in a zigzag pattern on a given number
+# of rows like this: (you may want to display this pattern in a fixed font for
+# better legibility)
+# 
+# 
+# P   A   H   N
+# A P L S I I G
+# Y   I   R
+# 
+# 
+# And then read line by line: "PAHNAPLSIIGYIR"
+# 
+# Write the code that will take a string and make this conversion given a
+# number of rows:
+# 
+# 
+# string convert(string s, int numRows);
+# 
+# Example 1:
+# 
+# 
+# Input: s = "PAYPALISHIRING", numRows = 3
+# Output: "PAHNAPLSIIGYIR"
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: s = "PAYPALISHIRING", numRows = 4
+# Output: "PINALSIGYAHRPI"
+# Explanation:
+# 
+# P     I    N
+# A   L S  I G
+# Y A   H R
+# P     I
+# 
+#
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        res = [''] * numRows
+        index, step = 0, 1
+        for c in s:
+            res[index] += c
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+
+        return ''.join(res)
