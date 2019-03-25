49e6e74c1c328ac7c634674c36636807c9d2f1cb
Wangdeyi <744731059@qq.com>
2019-03-25 12:07:41 +0800

    solution 24

---
+++ 24.swap-nodes-in-pairs.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=24 lang=python3
+#
+# [24] Swap Nodes in Pairs
+#
+# https://leetcode.com/problems/swap-nodes-in-pairs/description/
+#
+# algorithms
+# Medium (42.81%)
+# Total Accepted:    291.3K
+# Total Submissions: 667.9K
+# Testcase Example:  '[1,2,3,4]'
+#
+# Given a linked list, swap every two adjacent nodes and return its head.
+# 
+# You may not modify the values in the list's nodes, only nodes itself may be
+# changed.
+# 
+# 
+# 
+# Example:
+# 
+# 
+# Given 1->2->3->4, you should return the list as 2->1->4->3.
+# 
+# 
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        node = ListNode(0)
+        node.next = head
+        tmp = node
+        while head and head.next:
+            tmp.next = head.next
+            head.next = tmp.next.next
+            tmp.next.next = head
+            tmp = head
+            head = head.next
+        return node.next
