c3efda05cad21dfe8e0a38f09b212a4ccd8a8ad9
Wangdeyi <744731059@qq.com>
2019-04-01 10:34:45 +0800

    solution 46

---
+++ 46.permutations.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=46 lang=python3
+#
+# [46] Permutations
+#
+# https://leetcode.com/problems/permutations/description/
+#
+# algorithms
+# Medium (52.99%)
+# Total Accepted:    354.2K
+# Total Submissions: 655.1K
+# Testcase Example:  '[1,2,3]'
+#
+# Given a collection of distinct integers, return all possible permutations.
+# 
+# Example:
+# 
+# 
+# Input: [1,2,3]
+# Output:
+# [
+# ⁠ [1,2,3],
+# ⁠ [1,3,2],
+# ⁠ [2,1,3],
+# ⁠ [2,3,1],
+# ⁠ [3,1,2],
+# ⁠ [3,2,1]
+# ]
+# 
+# 
+#
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.dfs(nums, [], res)
+        return res
+
+    def dfs(self, nums, cur, res) -> None:
+        if not nums:
+            res.append(cur)
+        for i in range(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:], cur+[nums[i]], res)
