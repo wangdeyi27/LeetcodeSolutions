18847cea59b26e33e2f701d3d57b6966331d847d
Wangdeyi <744731059@qq.com>
2019-04-08 19:39:28 +0800

    solution 61

---
+++ 61.rotate-list.py
@@ -0,0 +1,70 @@
+#
+# @lc app=leetcode id=61 lang=python3
+#
+# [61] Rotate List
+#
+# https://leetcode.com/problems/rotate-list/description/
+#
+# algorithms
+# Medium (26.34%)
+# Total Accepted:    184.2K
+# Total Submissions: 687.5K
+# Testcase Example:  '[1,2,3,4,5]\n2'
+#
+# Given a linked list, rotate the list to the right by k places, where k is
+# non-negative.
+# 
+# Example 1:
+# 
+# 
+# Input: 1->2->3->4->5->NULL, k = 2
+# Output: 4->5->1->2->3->NULL
+# Explanation:
+# rotate 1 steps to the right: 5->1->2->3->4->NULL
+# rotate 2 steps to the right: 4->5->1->2->3->NULL
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: 0->1->2->NULL, k = 4
+# Output: 2->0->1->NULL
+# Explanation:
+# rotate 1 steps to the right: 2->0->1->NULL
+# rotate 2 steps to the right: 1->2->0->NULL
+# rotate 3 steps to the right: 0->1->2->NULL
+# rotate 4 steps to the right: 2->0->1->NULL
+# 
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        if not head or not head.next:
+            return head
+        node = head
+        length = 1
+        while node.next:
+            length += 1
+            node = node.next
+
+        times = k % length
+        if times == 0:
+            return head
+        
+        slow, fast = head, head
+        for _ in range(times):
+            fast = fast.next
+
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+
+        tmp = slow.next
+        slow.next = None
+        fast.next = head
+        return tmp
