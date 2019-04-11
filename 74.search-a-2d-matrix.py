3af023a679b7db55295a95b9541516aa4bc35137
Wangdeyi <744731059@qq.com>
2019-04-11 16:07:50 +0800

    solution 74

---
+++ 74.search-a-2d-matrix.py
@@ -0,0 +1,63 @@
+#
+# @lc app=leetcode id=74 lang=python3
+#
+# [74] Search a 2D Matrix
+#
+# https://leetcode.com/problems/search-a-2d-matrix/description/
+#
+# algorithms
+# Medium (34.63%)
+# Total Accepted:    214.8K
+# Total Submissions: 618.1K
+# Testcase Example:  '[[1,3,5,7],[10,11,16,20],[23,30,34,50]]\n3'
+#
+# Write an efficient algorithm that searches for a value in an m x n matrix.
+# This matrix has the following properties:
+# 
+# 
+# Integers in each row are sorted from left to right.
+# The first integer of each row is greater than the last integer of the
+# previous row.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input:
+# matrix = [
+# ⁠ [1,   3,  5,  7],
+# ⁠ [10, 11, 16, 20],
+# ⁠ [23, 30, 34, 50]
+# ]
+# target = 3
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input:
+# matrix = [
+# ⁠ [1,   3,  5,  7],
+# ⁠ [10, 11, 16, 20],
+# ⁠ [23, 30, 34, 50]
+# ]
+# target = 13
+# Output: false
+# 
+#
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+        row, col = len(matrix), len(matrix[0])
+        low, high = 0, row * col - 1
+        while low < high:
+            mid = (low + high) // 2
+            if matrix[mid//col][mid%col] == target:
+                return True
+            elif matrix[mid//col][mid%col] < target:
+                low = mid + 1
+            else:
+                high = mid
+        return matrix[high//col][high%col] == target
