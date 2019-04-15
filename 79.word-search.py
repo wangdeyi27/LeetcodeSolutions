9a72540cd239bf43cd875e151ccf929def98a134
Wangdeyi <744731059@qq.com>
2019-04-15 16:00:13 +0800

    solution 79

---
+++ 79.word-search.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode id=79 lang=python3
+#
+# [79] Word Search
+#
+# https://leetcode.com/problems/word-search/description/
+#
+# algorithms
+# Medium (30.17%)
+# Total Accepted:    267.6K
+# Total Submissions: 867.9K
+# Testcase Example:  '[["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]\n"ABCCED"'
+#
+# Given a 2D board and a word, find if the word exists in the grid.
+# 
+# The word can be constructed from letters of sequentially adjacent cell, where
+# "adjacent" cells are those horizontally or vertically neighboring. The same
+# letter cell may not be used more than once.
+# 
+# Example:
+# 
+# 
+# board =
+# [
+# ⁠ ['A','B','C','E'],
+# ⁠ ['S','F','C','S'],
+# ⁠ ['A','D','E','E']
+# ]
+# 
+# Given word = "ABCCED", return true.
+# Given word = "SEE", return true.
+# Given word = "ABCB", return false.
+# 
+# 
+#
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        if not board:
+            return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.dfs(board, i, j, word):
+                    return True
+        return False
+
+    def dfs(self, board, i, j, word):
+        if len(word) == 0:
+            return True
+        if i<0 or i>=len(board) or j<0 or j>=len(board[0]) or word[0]!=board[i][j]:
+            return False
+        cur = board[i][j]
+        board[i][j] = '#'
+        res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) \
+              or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
+        board[i][j] = cur
+        return res
