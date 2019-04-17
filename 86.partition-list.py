527252c6e119bdfd9ad070ca163e3431728b862c
Wangdeyi <744731059@qq.com>
2019-04-17 11:47:25 +0800

    solution 86

---
+++ 86.partition-list.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode id=86 lang=python3
+#
+# [86] Partition List
+#
+# https://leetcode.com/problems/partition-list/description/
+#
+# algorithms
+# Medium (36.08%)
+# Total Accepted:    158.8K
+# Total Submissions: 431.4K
+# Testcase Example:  '[1,4,3,2,5,2]\n3'
+#
+# Given a linked list and a value x, partition it such that all nodes less than
+# x come before nodes greater than or equal to x.
+# 
+# You should preserve the original relative order of the nodes in each of the
+# two partitions.
+# 
+# Example:
+# 
+# 
+# Input: head = 1->4->3->2->5->2, x = 3
+# Output: 1->2->2->4->3->5
+# 
+# 
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        left_head = left = ListNode(0)
+        right_head = right = ListNode(0)
+        while head:
+            if head.val < x:
+                left.next = head
+                left = left.next
+            else:
+                right.next = head
+                right = right.next
+            head = head.next
+        right.next = None
+        left.next = right_head.next
+
+        return left_head.next
