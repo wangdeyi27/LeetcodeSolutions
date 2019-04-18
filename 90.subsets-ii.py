5dfd53e2b4b9dedc1ba78f98da5e7bf2e29d0aee
Wangdeyi <744731059@qq.com>
2019-04-18 11:35:46 +0800

    solution 90

--- 90.subsets-ii.py
+++ 90.subsets-ii.py
@@ -39,7 +39,8 @@ class Solution:
         return res
 
     def dfs(self, nums, index, path, res):
-        if path not in res:
-            res.append(path)
+        res.append(path)
         for i in range(index, len(nums)):
+            if i > index and nums[i] == nums[i-1]:
+                continue
             self.dfs(nums, i+1, path+[nums[i]], res)
