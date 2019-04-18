2ab4248a82913026ab135455efc33533b5b977c0
Wangdeyi <744731059@qq.com>
2019-04-18 11:31:23 +0800

    solution 90

---
+++ 90.subsets-ii.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=90 lang=python3
+#
+# [90] Subsets II
+#
+# https://leetcode.com/problems/subsets-ii/description/
+#
+# algorithms
+# Medium (41.21%)
+# Total Accepted:    196.6K
+# Total Submissions: 468.4K
+# Testcase Example:  '[1,2,2]'
+#
+# Given a collection of integers that might contain duplicates, nums, return
+# all possible subsets (the power set).
+# 
+# Note: The solution set must not contain duplicate subsets.
+# 
+# Example:
+# 
+# 
+# Input: [1,2,2]
+# Output:
+# [
+# ⁠ [2],
+# ⁠ [1],
+# ⁠ [1,2,2],
+# ⁠ [2,2],
+# ⁠ [1,2],
+# ⁠ []
+# ]
+# 
+# 
+#
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.dfs(sorted(nums), 0, [], res)
+        return res
+
+    def dfs(self, nums, index, path, res):
+        if path not in res:
+            res.append(path)
+        for i in range(index, len(nums)):
+            self.dfs(nums, i+1, path+[nums[i]], res)
