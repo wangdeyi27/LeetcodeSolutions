a4955b3c317b0ee876ae1bb207c05acbeff83d48
Wangdeyi <744731059@qq.com>
2019-04-22 14:09:47 +0800

    solution 95

---
+++ 95.unique-binary-search-trees-ii.py
@@ -0,0 +1,63 @@
+#
+# @lc app=leetcode id=95 lang=python3
+#
+# [95] Unique Binary Search Trees II
+#
+# https://leetcode.com/problems/unique-binary-search-trees-ii/description/
+#
+# algorithms
+# Medium (34.59%)
+# Total Accepted:    134.4K
+# Total Submissions: 380.5K
+# Testcase Example:  '3'
+#
+# Given an integer n, generate all structurally unique BST's (binary search
+# trees) that store values 1 ... n.
+# 
+# Example:
+# 
+# 
+# Input: 3
+# Output:
+# [
+# [1,null,3,2],
+# [3,2,null,1],
+# [3,1,null,null,2],
+# [2,1,3],
+# [1,null,2,null,3]
+# ]
+# Explanation:
+# The above output corresponds to the 5 unique BST's shown below:
+# 
+# ⁠  1         3     3      2      1
+# ⁠   \       /     /      / \      \
+# ⁠    3     2     1      1   3      2
+# ⁠   /     /       \                 \
+# ⁠  2     1         2                 3
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def generateTrees(self, n: int) -> List[TreeNode]:
+        if n == 0:
+            return []
+        return self.dfs(1, n+1)
+
+    def dfs(self, start, end):
+        if start == end:
+            return None
+        res = []
+        for i in range(start, end):
+            for l in self.dfs(start, i) or [None]:
+                for r in self.dfs(i+1, end) or [None]:
+                    node = TreeNode(i)
+                    node.left, node.right = l, r
+                    res.append(node)
+        return res
