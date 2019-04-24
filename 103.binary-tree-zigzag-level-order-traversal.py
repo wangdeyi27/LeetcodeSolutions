60e1faf44a4094727b85a97a67498f500853fe81
Wangdeyi <744731059@qq.com>
2019-04-24 20:18:34 +0800

    solution 103

---
+++ 103.binary-tree-zigzag-level-order-traversal.py
@@ -0,0 +1,58 @@
+#
+# @lc app=leetcode id=103 lang=python3
+#
+# [103] Binary Tree Zigzag Level Order Traversal
+#
+# https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+#
+# algorithms
+# Medium (40.17%)
+# Total Accepted:    210.2K
+# Total Submissions: 509.1K
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# Given a binary tree, return the zigzag level order traversal of its nodes'
+# values. (ie, from left to right, then right to left for the next level and
+# alternate between).
+# 
+# 
+# For example:
+# Given binary tree [3,9,20,null,null,15,7],
+# 
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+# 
+# 
+# 
+# return its zigzag level order traversal as:
+# 
+# [
+# ⁠ [3],
+# ⁠ [20,9],
+# ⁠ [15,7]
+# ]
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        res, level, step = [], [root], False
+        while root and level:
+            if step:
+                res.append([node.val for node in level[::-1]])
+            else:
+                res.append([node.val for node in level])
+            LRpair = [(node.left, node.right) for node in level]
+            level = [leaf for LR in LRpair for leaf in LR if leaf]
+            step = not step
+        return res
