d8159248d16b4bd97eda6daabc02392ef995be5a
Wangdeyi <744731059@qq.com>
2019-04-25 15:54:36 +0800

    solution 106

---
+++ 106.construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -0,0 +1,50 @@
+#
+# @lc app=leetcode id=106 lang=python3
+#
+# [106] Construct Binary Tree from Inorder and Postorder Traversal
+#
+# https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/description/
+#
+# algorithms
+# Medium (37.65%)
+# Total Accepted:    148.6K
+# Total Submissions: 383K
+# Testcase Example:  '[9,3,15,20,7]\n[9,15,7,20,3]'
+#
+# Given inorder and postorder traversal of a tree, construct the binary tree.
+# 
+# Note:
+# You may assume that duplicates do not exist in the tree.
+# 
+# For example, given
+# 
+# 
+# inorder = [9,3,15,20,7]
+# postorder = [9,15,7,20,3]
+# 
+# Return the following binary tree:
+# 
+# 
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if inorder and postorder:
+            ind = inorder.index(postorder.pop())
+            root = TreeNode(inorder[ind])
+            root.right = self.buildTree(inorder[ind+1:], postorder)
+            root.left = self.buildTree(inorder[:ind], postorder)
+            return root
