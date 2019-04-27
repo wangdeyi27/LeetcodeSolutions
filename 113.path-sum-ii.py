541386235a1348f754738ec671d83a42e1da02b5
Wangdeyi <744731059@qq.com>
2019-04-27 19:37:59 +0800

    solution 113

---
+++ 113.path-sum-ii.py
@@ -0,0 +1,64 @@
+#
+# @lc app=leetcode id=113 lang=python3
+#
+# [113] Path Sum II
+#
+# https://leetcode.com/problems/path-sum-ii/description/
+#
+# algorithms
+# Medium (39.18%)
+# Total Accepted:    225.1K
+# Total Submissions: 559.5K
+# Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,5,1]\n22'
+#
+# Given a binary tree and a sum, find all root-to-leaf paths where each path's
+# sum equals the given sum.
+# 
+# Note: A leaf is a node with no children.
+# 
+# Example:
+# 
+# Given the below binary tree and sum = 22,
+# 
+# 
+# ⁠     5
+# ⁠    / \
+# ⁠   4   8
+# ⁠  /   / \
+# ⁠ 11  13  4
+# ⁠/  \    / \
+# 7    2  5   1
+# 
+# 
+# Return:
+# 
+# 
+# [
+# ⁠  [5,4,11,2],
+# ⁠  [5,8,4,5]
+# ]
+# 
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        res = []
+        self.dfs(root, sum, [], res)
+        return res
+
+    def dfs(self, root, sum, path, res):
+        if not root:
+            return
+        if sum == root.val and not root.left and not root.right:
+            res.append(path+[root.val])
+        if root.left:
+            self.dfs(root.left, sum-root.val, path+[root.val], res)
+        if root.right:
+            self.dfs(root.right, sum-root.val, path+[root.val], res)
