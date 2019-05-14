6958dbab7b269d04e88690f21a6508a94155965b
Wangdeyi <744731059@qq.com>
2019-05-14 22:53:00 +0800

    solution 143

---
+++ 143.reorder-list.py
@@ -0,0 +1,51 @@
+#
+# @lc app=leetcode id=143 lang=python3
+#
+# [143] Reorder List
+#
+# https://leetcode.com/problems/reorder-list/description/
+#
+# algorithms
+# Medium (29.58%)
+# Total Accepted:    152.4K
+# Total Submissions: 497.3K
+# Testcase Example:  '[1,2,3,4]'
+#
+# Given a singly linked list L: L0→L1→…→Ln-1→Ln,
+# reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+# 
+# You may not modify the values in the list's nodes, only nodes itself may be
+# changed.
+# 
+# Example 1:
+# 
+# 
+# Given 1->2->3->4, reorder it to 1->4->2->3.
+# 
+# Example 2:
+# 
+# 
+# Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
+# 
+# 
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head: return
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+        pre, slow.next, slow = None, None, slow.next
+        while slow:
+            slow.next, pre, slow = pre, slow, slow.next
+        while head and pre:
+            head.next, head, pre.next, pre = pre, head.next, head.next, pre.next 
