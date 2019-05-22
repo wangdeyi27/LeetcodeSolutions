6bb58bb2105152d5d4c456c2d3230e41f60d1071
Wangdeyi <744731059@qq.com>
2019-05-22 11:33:41 +0800

    solution 177

---
+++ 177.nth-highest-salary.sql
@@ -0,0 +1,47 @@
+--
+-- @lc app=leetcode id=177 lang=mysql
+--
+-- [177] Nth Highest Salary
+--
+-- https://leetcode.com/problems/nth-highest-salary/description/
+--
+-- database
+-- Medium (24.51%)
+-- Total Accepted:    64.4K
+-- Total Submissions: 246.4K
+-- Testcase Example:  '{"headers": {"Employee": ["Id", "Salary"]}, "argument": 2, "rows": {"Employee": [[1, 100], [2, 200], [3, 300]]}}'
+--
+-- Write a SQL query to get the nth highest salary from the Employee table.
+-- 
+-- 
+-- +----+--------+
+-- | Id | Salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- 
+-- 
+-- For example, given the above Employee table, the nth highest salary where n
+-- = 2 is 200. If there is no nth highest salary, then the query should return
+-- null.
+-- 
+-- 
+-- +------------------------+
+-- | getNthHighestSalary(2) |
+-- +------------------------+
+-- | 200                    |
+-- +------------------------+
+-- 
+-- 
+--
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE M INT;
+SET M=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT M, 1
+  );
+END
