63cc23d700ca7344c4d5ff20ca65480df876d813
Wangdeyi <744731059@qq.com>
2019-05-23 11:51:26 +0800

    solution 179

---
+++ 179.largest-number.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=179 lang=python3
+#
+# [179] Largest Number
+#
+# https://leetcode.com/problems/largest-number/description/
+#
+# algorithms
+# Medium (25.12%)
+# Total Accepted:    128.7K
+# Total Submissions: 499.8K
+# Testcase Example:  '[10,2]'
+#
+# Given a list of non negative integers, arrange them such that they form the
+# largest number.
+# 
+# Example 1:
+# 
+# 
+# Input: [10,2]
+# Output: "210"
+# 
+# Example 2:
+# 
+# 
+# Input: [3,30,34,5,9]
+# Output: "9534330"
+# 
+# 
+# Note: The result may be very large, so you need to return a string instead of
+# an integer.
+# 
+#
+class LargerNumKey(str):
+    def __lt__(x, y):
+        return x+y > y+x
+        
+class Solution:
+    def largestNumber(self, nums):
+        largest_num = ''.join(sorted(map(str, nums), key=LargerNumKey))
+        return '0' if largest_num[0] == '0' else largest_num
