196c2f88ce6ae10e1729ffdde461a8cdf6d7a219
Wangdeyi <744731059@qq.com>
2019-05-27 14:34:49 +0800

    solution 199

---
+++ 199.binary-tree-right-side-view.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=199 lang=python3
+#
+# [199] Binary Tree Right Side View
+#
+# https://leetcode.com/problems/binary-tree-right-side-view/description/
+#
+# algorithms
+# Medium (46.31%)
+# Total Accepted:    167.2K
+# Total Submissions: 349.2K
+# Testcase Example:  '[1,2,3,null,5,null,4]'
+#
+# Given a binary tree, imagine yourself standing on the right side of it,
+# return the values of the nodes you can see ordered from top to bottom.
+# 
+# Example:
+# 
+# 
+# Input: [1,2,3,null,5,null,4]
+# Output: [1, 3, 4]
+# Explanation:
+# 
+# ⁠  1            <---
+# ⁠/   \
+# 2     3         <---
+# ⁠\     \
+# ⁠ 5     4       <---
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        res, lev = [], [root]
+        while root and lev:
+             res.append(lev[-1].val)
+             lev = [k for n in lev for k in (n.left, n.right) if k]
+        return res       
