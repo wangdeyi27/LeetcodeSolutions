a5a98090c85f4ae474b2be1dfb30b2666e56a133
Wangdeyi <744731059@qq.com>
2019-05-28 10:58:14 +0800

    solution 200

---
+++ 200.number-of-islands.py
@@ -0,0 +1,62 @@
+#
+# @lc app=leetcode id=200 lang=python3
+#
+# [200] Number of Islands
+#
+# https://leetcode.com/problems/number-of-islands/description/
+#
+# algorithms
+# Medium (39.99%)
+# Total Accepted:    353.3K
+# Total Submissions: 851.8K
+# Testcase Example:  '[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]'
+#
+# Given a 2d grid map of '1's (land) and '0's (water), count the number of
+# islands. An island is surrounded by water and is formed by connecting
+# adjacent lands horizontally or vertically. You may assume all four edges of
+# the grid are all surrounded by water.
+# 
+# Example 1:
+# 
+# 
+# Input:
+# 11110
+# 11010
+# 11000
+# 00000
+# 
+# Output: 1
+# 
+# 
+# Example 2:
+# 
+# 
+# Input:
+# 11000
+# 11000
+# 00100
+# 00011
+# 
+# Output: 3
+# 
+#
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, i, j):
+        if not (0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == '1'):
+            return
+        grid[i][j] = '#'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)       
