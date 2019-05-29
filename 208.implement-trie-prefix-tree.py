3a2c53ea2403ee5fe05ce2bc120a9ee71d88dcea
Wangdeyi <744731059@qq.com>
2019-05-29 14:19:36 +0800

    solution 208

---
+++ 208.implement-trie-prefix-tree.py
@@ -0,0 +1,89 @@
+#
+# @lc app=leetcode id=208 lang=python3
+#
+# [208] Implement Trie (Prefix Tree)
+#
+# https://leetcode.com/problems/implement-trie-prefix-tree/description/
+#
+# algorithms
+# Medium (36.37%)
+# Total Accepted:    178.5K
+# Total Submissions: 464.5K
+# Testcase Example:  '["Trie","insert","search","search","startsWith","insert","search"]\n[[],["apple"],["apple"],["app"],["app"],["app"],["app"]]'
+#
+# Implement a trie with insert, search, and startsWith methods.
+# 
+# Example:
+# 
+# 
+# Trie trie = new Trie();
+# 
+# trie.insert("apple");
+# trie.search("apple");   // returns true
+# trie.search("app");     // returns false
+# trie.startsWith("app"); // returns true
+# trie.insert("app");   
+# trie.search("app");     // returns true
+# 
+# 
+# Note:
+# 
+# 
+# You may assume that all inputs are consist of lowercase letters a-z.
+# All inputs are guaranteed to be non-empty strings.
+# 
+# 
+#
+from collections import defaultdict
+class TrieNode:
+
+    def __init__(self):
+        self.nodes = defaultdict(TrieNode)
+        self.isword = False
+
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        curr = self.root
+        for char in word:
+            curr = curr.nodes[char]
+        curr.isword = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        curr = self.root
+        for char in word:
+            if char not in curr.nodes:
+                return False
+            curr = curr.nodes[char]
+        return curr.isword
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        curr = self.root
+        for char in prefix:
+            if char not in curr.nodes:
+                return False
+            curr = curr.nodes[char]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
