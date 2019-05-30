79d4667fad06ac65d8eb83c98fc69d6085400fb5
Wangdeyi <744731059@qq.com>
2019-05-30 14:51:04 +0800

    solution 210

---
+++ 210.course-schedule-ii.py
@@ -0,0 +1,95 @@
+#
+# @lc app=leetcode id=210 lang=python3
+#
+# [210] Course Schedule II
+#
+# https://leetcode.com/problems/course-schedule-ii/description/
+#
+# algorithms
+# Medium (33.40%)
+# Total Accepted:    147.3K
+# Total Submissions: 423.1K
+# Testcase Example:  '2\n[[1,0]]'
+#
+# There are a total of n courses you have to take, labeled from 0 to n-1.
+# 
+# Some courses may have prerequisites, for example to take course 0 you have to
+# first take course 1, which is expressed as a pair: [0,1]
+# 
+# Given the total number of courses and a list of prerequisite pairs, return
+# the ordering of courses you should take to finish all courses.
+# 
+# There may be multiple correct orders, you just need to return one of them. If
+# it is impossible to finish all courses, return an empty array.
+# 
+# Example 1:
+# 
+# 
+# Input: 2, [[1,0]] 
+# Output: [0,1]
+# Explanation: There are a total of 2 courses to take. To take course 1 you
+# should have finished   
+# course 0. So the correct course order is [0,1] .
+# 
+# Example 2:
+# 
+# 
+# Input: 4, [[1,0],[2,0],[3,1],[3,2]]
+# Output: [0,1,2,3] or [0,2,1,3]
+# Explanation: There are a total of 4 courses to take. To take course 3 you
+# should have finished both     
+# ⁠            courses 1 and 2. Both courses 1 and 2 should be taken after you
+# finished course 0. 
+# So one correct course order is [0,1,2,3]. Another correct ordering is
+# [0,2,1,3] .
+# 
+# Note:
+# 
+# 
+# The input prerequisites is a graph represented by a list of edges, not
+# adjacency matrices. Read more about how a graph is represented.
+# You may assume that there are no duplicate edges in the input prerequisites.
+# 
+# 
+#
+from collections import defaultdict
+class Solution:
+
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+
+        # Prepare the graph
+        adj_list = defaultdict(list)
+        indegree = {}
+        for dest, src in prerequisites:
+            adj_list[src].append(dest)
+
+            # Record each node's in-degree
+            indegree[dest] = indegree.get(dest, 0) + 1
+
+        # Queue for maintainig list of nodes that have 0 in-degree
+        zero_indegree_queue = [k for k in range(numCourses) if k not in indegree]
+
+        topological_sorted_order = []
+
+        # Until there are nodes in the Q
+        while zero_indegree_queue:
+
+            # Pop one node with 0 in-degree
+            vertex = zero_indegree_queue.pop(0)
+            topological_sorted_order.append(vertex)
+
+            # Reduce in-degree for all the neighbors
+            if vertex in adj_list:
+                for neighbor in adj_list[vertex]:
+                    indegree[neighbor] -= 1
+
+                    # Add neighbor to Q if in-degree becomes 0
+                    if indegree[neighbor] == 0:
+                        zero_indegree_queue.append(neighbor)
+
+        return topological_sorted_order if len(topological_sorted_order) == numCourses else []
