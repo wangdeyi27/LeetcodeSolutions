ff7ed84e42329b6a30c3220e26d7bcc958b066a7
Wangdeyi <744731059@qq.com>
2019-06-06 19:28:15 +0800

    solution 222

---
+++ 222.count-complete-tree-nodes.py
@@ -0,0 +1,55 @@
+#
+# @lc app=leetcode id=222 lang=python3
+#
+# [222] Count Complete Tree Nodes
+#
+# https://leetcode.com/problems/count-complete-tree-nodes/description/
+#
+# algorithms
+# Medium (30.96%)
+# Total Accepted:    121.5K
+# Total Submissions: 360K
+# Testcase Example:  '[1,2,3,4,5,6]'
+#
+# Given a complete binary tree, count the number of nodes.
+# 
+# Note: 
+# 
+# Definition of a complete binary tree from Wikipedia:
+# In a complete binary tree every level, except possibly the last, is
+# completely filled, and all nodes in the last level are as far left as
+# possible. It can have between 1 and 2h nodes inclusive at the last level h.
+# 
+# Example:
+# 
+# 
+# Input: 
+# ⁠   1
+# ⁠  / \
+# ⁠ 2   3
+# ⁠/ \  /
+# 4  5 6
+# 
+# Output: 6
+# 
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        if not root: return 0
+        left = self.getDepth(root.left)
+        right = self.getDepth(root.right)
+        if left == right:
+            return pow(2, left) + self.countNodes(root.right)
+        else:
+            return pow(2, right) + self.countNodes(root.left)
+
+    def getDepth(self, root):
+        if not root: return 0
+        return 1 + self.getDepth(root.left) 
