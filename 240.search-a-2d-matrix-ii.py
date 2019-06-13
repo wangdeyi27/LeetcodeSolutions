b3ae4c54678045fa0c8f6f147a9b07dd8ff1b2b8
Wangdeyi <744731059@qq.com>
2019-06-13 12:02:07 +0800

    solution 240

---
+++ 240.search-a-2d-matrix-ii.py
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode id=240 lang=python3
+#
+# [240] Search a 2D Matrix II
+#
+# https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+#
+# algorithms
+# Medium (40.12%)
+# Total Accepted:    182.6K
+# Total Submissions: 446.2K
+# Testcase Example:  '[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]\n5'
+#
+# Write an efficient algorithm that searches for a value in an m x n matrix.
+# This matrix has the following properties:
+# 
+# 
+# Integers in each row are sorted in ascending from left to right.
+# Integers in each column are sorted in ascending from top to bottom.
+# 
+# 
+# Example:
+# 
+# Consider the following matrix:
+# 
+# 
+# [
+# ⁠ [1,   4,  7, 11, 15],
+# ⁠ [2,   5,  8, 12, 19],
+# ⁠ [3,   6,  9, 16, 22],
+# ⁠ [10, 13, 14, 17, 24],
+# ⁠ [18, 21, 23, 26, 30]
+# ]
+# 
+# 
+# Given target = 5, return true.
+# 
+# Given target = 20, return false.
+# 
+#
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for row in matrix:
+            for i in range(len(row)-1, -1, -1):
+                if target == row[i]: return True
+                if target > row[i]: break
+        return False
