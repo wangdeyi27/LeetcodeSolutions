44a23cb827b6335f0af71216b485cd5cc6988775
Wangdeyi <744731059@qq.com>
2019-07-23 15:40:50 +0800

    solution 57

---
+++ 57.insert-interval.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode id=57 lang=python3
+#
+# [57] Insert Interval
+#
+# https://leetcode.com/problems/insert-interval/description/
+#
+# algorithms
+# Hard (30.62%)
+# Total Accepted:    184.9K
+# Total Submissions: 587.2K
+# Testcase Example:  '[[1,3],[6,9]]\n[2,5]'
+#
+# Given a set of non-overlapping intervals, insert a new interval into the
+# intervals (merge if necessary).
+# 
+# You may assume that the intervals were initially sorted according to their
+# start times.
+# 
+# Example 1:
+# 
+# 
+# Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+# Output: [[1,5],[6,9]]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+# Output: [[1,2],[3,10],[12,16]]
+# Explanation: Because the new interval [4,8] overlaps with
+# [3,5],[6,7],[8,10].
+# 
+# NOTE: input types have been changed on April 15, 2019. Please reset to
+# default code definition to get new method signature.
+# 
+#
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        s, e = newInterval[0], newInterval[1]
+        left, right = [], []
+        for i in intervals:
+            if i[1] < s: left.append(i)
+            elif i[0] > e: right.append(i)
+            else:
+                s = min(i[0], s)
+                e = max(i[1], e)
+        return left + [[s, e]] + right
