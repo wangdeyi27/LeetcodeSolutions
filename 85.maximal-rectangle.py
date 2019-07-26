05f78d4eb41b8f29a7366dd21b9ad416ee4fe6d2
Wangdeyi <744731059@qq.com>
2019-07-26 14:58:32 +0800

    solution 85

---
+++ 85.maximal-rectangle.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode id=85 lang=python3
+#
+# [85] Maximal Rectangle
+#
+# https://leetcode.com/problems/maximal-rectangle/description/
+#
+# algorithms
+# Hard (32.20%)
+# Total Accepted:    128.2K
+# Total Submissions: 377.3K
+# Testcase Example:  '[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]'
+#
+# Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle
+# containing only 1's and return its area.
+# 
+# Example:
+# 
+# 
+# Input:
+# [
+# ⁠ ["1","0","1","0","0"],
+# ⁠ ["1","0","1","1","1"],
+# ⁠ ["1","1","1","1","1"],
+# ⁠ ["1","0","0","1","0"]
+# ]
+# Output: 6
+# 
+# 
+#
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]: return 0
+        n = len(matrix[0])
+        height = [0] * (n + 1)
+        res = 0
+        for row in matrix:
+            for i in range(n):
+                height[i] = height[i] + 1 if row[i] == '1' else 0
+            stack = [-1]
+            for i in range(n + 1):
+                while height[i] < height[stack[-1]]:
+                    h = height[stack.pop()]
+                    w = i - stack[-1] - 1
+                    res = max(res, h*w)
+                stack.append(i)
+        return res
