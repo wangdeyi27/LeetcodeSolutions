cd2f2d79018d99f0bd379aa151b76e61d42dd20a
Wangdeyi <744731059@qq.com>
2019-07-26 14:19:53 +0800

    solution 84

---
+++ 84.largest-rectangle-in-histogram.py
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode id=84 lang=python3
+#
+# [84] Largest Rectangle in Histogram
+#
+# https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+#
+# algorithms
+# Hard (30.05%)
+# Total Accepted:    184.8K
+# Total Submissions: 582.8K
+# Testcase Example:  '[2,1,5,6,2,3]'
+#
+# Given n non-negative integers representing the histogram's bar height where
+# the width of each bar is 1, find the area of largest rectangle in the
+# histogram.
+# 
+# 
+# 
+# 
+# Above is a histogram where width of each bar is 1, given height =
+# [2,1,5,6,2,3].
+# 
+# 
+# 
+# 
+# The largest rectangle is shown in the shaded area, which has area = 10
+# unit.
+# 
+# 
+# 
+# Example:
+# 
+# 
+# Input: [2,1,5,6,2,3]
+# Output: 10
+# 
+# 
+#
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        heights.append(0)
+        stack = [-1]
+        res = 0
+        for i in range(len(heights)):
+            while heights[i] < heights[stack[-1]]:
+                h = heights[stack.pop()]
+                w = i - stack[-1] - 1
+                res = max(res, h*w)
+            stack.append(i)
+        heights.pop()
+        return res
