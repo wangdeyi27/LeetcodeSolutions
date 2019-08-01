58e70bfa080e5f1cc42f40a5d243069bcc884fb2
Wangdeyi <744731059@qq.com>
2019-08-01 15:38:27 +0800

    solution 115

--- 115.distinct-subsequences.py
+++ 115.distinct-subsequences.py
@@ -62,11 +62,11 @@
 #
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        l1, l2 = len(s)+1, len(t)+1
-        cur = [0] * l2
-        cur[0] = 1
-        for i in range(1, l1):
-            pre = cur[:]
-            for j in range(1, l2):
-                cur[j] = pre[j] + pre[j-1]*(s[i-1] == t[j-1])
-        return cur[-1]       
+        ls, lt = len(s), len(t)
+        dp = [0] * (lt + 1)
+        dp[0] = 1
+        for i in range(1, ls + 1):
+            for j in range(min(i, lt), 0, -1):
+                if s[i - 1] == t[j - 1]:
+                    dp[j] = dp[j] + dp[j - 1]
+        return dp[-1]
