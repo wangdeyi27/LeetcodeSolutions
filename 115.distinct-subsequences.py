b5f2452a7e86b6c71eb6a251e5ad260028617fe5
Wangdeyi <744731059@qq.com>
2019-08-01 15:20:30 +0800

    solution 115

---
+++ 115.distinct-subsequences.py
@@ -0,0 +1,72 @@
+#
+# @lc app=leetcode id=115 lang=python3
+#
+# [115] Distinct Subsequences
+#
+# https://leetcode.com/problems/distinct-subsequences/description/
+#
+# algorithms
+# Hard (34.41%)
+# Total Accepted:    110.6K
+# Total Submissions: 311.9K
+# Testcase Example:  '"rabbbit"\n"rabbit"'
+#
+# Given a string S and a string T, count the number of distinct subsequences of
+# S which equals T.
+# 
+# A subsequence of a string is a new string which is formed from the original
+# string by deleting some (can be none) of the characters without disturbing
+# the relative positions of the remaining characters. (ie, "ACE" is a
+# subsequence of "ABCDE" while "AEC" is not).
+# 
+# Example 1:
+# 
+# 
+# Input: S = "rabbbit", T = "rabbit"
+# Output: 3
+# Explanation:
+# 
+# As shown below, there are 3 ways you can generate "rabbit" from S.
+# (The caret symbol ^ means the chosen letters)
+# 
+# rabbbit
+# ^^^^ ^^
+# rabbbit
+# ^^ ^^^^
+# rabbbit
+# ^^^ ^^^
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: S = "babgbag", T = "bag"
+# Output: 5
+# Explanation:
+# 
+# As shown below, there are 5 ways you can generate "bag" from S.
+# (The caret symbol ^ means the chosen letters)
+# 
+# babgbag
+# ^^ ^
+# babgbag
+# ^^    ^
+# babgbag
+# ^    ^^
+# babgbag
+# ⁠ ^  ^^
+# babgbag
+# ⁠   ^^^
+# 
+# 
+#
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        l1, l2 = len(s)+1, len(t)+1
+        cur = [0] * l2
+        cur[0] = 1
+        for i in range(1, l1):
+            pre = cur[:]
+            for j in range(1, l2):
+                cur[j] = pre[j] + pre[j-1]*(s[i-1] == t[j-1])
+        return cur[-1]       
