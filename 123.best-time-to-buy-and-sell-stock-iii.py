470e9e656894af26873f9d97848caf150a989167
Wangdeyi <744731059@qq.com>
2019-08-02 11:44:20 +0800

    solution 123

--- 123.best-time-to-buy-and-sell-stock-iii.py
+++ 123.best-time-to-buy-and-sell-stock-iii.py
@@ -58,8 +58,8 @@ class Solution:
         one_buy = two_buy = sys.maxsize
         one_profit = two_profit = 0
         for p in prices:
-            one_buy = min(one_buy,p)
-            one_profit = max(one_profit,p - one_buy)
-            two_buy = min(two_buy,p - one_profit)
-            two_profit = max(two_profit,p - two_buy)
+            one_buy = min(one_buy, p)
+            one_profit = max(one_profit, p - one_buy)
+            two_buy = min(two_buy, p - one_profit)
+            two_profit = max(two_profit, p - two_buy)
         return two_profit
