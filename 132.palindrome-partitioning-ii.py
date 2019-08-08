d291190c3599108632c52d05ce732960220a1244
Wangdeyi <744731059@qq.com>
2019-08-08 11:52:06 +0800

    solution 132

---
+++ 132.palindrome-partitioning-ii.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode id=132 lang=python3
+#
+# [132] Palindrome Partitioning II
+#
+# https://leetcode.com/problems/palindrome-partitioning-ii/description/
+#
+# algorithms
+# Hard (26.58%)
+# Total Accepted:    107.9K
+# Total Submissions: 385.6K
+# Testcase Example:  '"aab"'
+#
+# Given a string s, partition s such that every substring of the partition is a
+# palindrome.
+# 
+# Return the minimum cuts needed for a palindrome partitioning of s.
+# 
+# Example:
+# 
+# 
+# Input: "aab"
+# Output: 1
+# Explanation: The palindrome partitioning ["aa","b"] could be produced using 1
+# cut.
+# 
+# 
+#
+class Solution:
+    def minCut(self, s: str) -> int:
+        if s == s[::-1]: return 0
+        for i in range(1, len(s)):
+            if s[:i] == s[:i][::-1] and s[i:] == s[i:][::-1]:
+                return 1
+        cut = [x for x in range(-1,len(s))]  # cut numbers in worst case (no palindrome)
+        for i in range(len(s)):
+            r1, r2 = 0, 0
+            # use i as origin, and gradually enlarge radius if a palindrome exists
+            # odd palindrome
+            while i-r1 >= 0 and i+r1 < len(s) and s[i-r1] == s[i+r1]:
+                cut[i+r1+1] = min(cut[i+r1+1], cut[i-r1]+1)
+                r1 += 1
+            # even palindrome
+            while i-r2 >= 0 and i+r2+1 < len(s) and s[i-r2] == s[i+r2+1]:
+                cut[i+r2+2] = min(cut[i+r2+2], cut[i-r2]+1)
+                r2 += 1
+        return cut[-1]
