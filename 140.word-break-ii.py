c0711fd6afd9d95ea2cdb091fcaed5f68de810a8
Wangdeyi <744731059@qq.com>
2019-08-12 14:34:13 +0800

    solution 140

---
+++ 140.word-break-ii.py
@@ -0,0 +1,84 @@
+#
+# @lc app=leetcode id=140 lang=python3
+#
+# [140] Word Break II
+#
+# https://leetcode.com/problems/word-break-ii/description/
+#
+# algorithms
+# Hard (26.51%)
+# Total Accepted:    168.5K
+# Total Submissions: 604.1K
+# Testcase Example:  '"catsanddog"\n["cat","cats","and","sand","dog"]'
+#
+# Given a non-empty string s and a dictionary wordDict containing a list of
+# non-empty words, add spaces in s to construct a sentence where each word is a
+# valid dictionary word. Return all such possible sentences.
+# 
+# Note:
+# 
+# 
+# The same word in the dictionary may be reused multiple times in the
+# segmentation.
+# You may assume the dictionary does not contain duplicate words.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input:
+# s = "catsanddog"
+# wordDict = ["cat", "cats", "and", "sand", "dog"]
+# Output:
+# [
+# "cats and dog",
+# "cat sand dog"
+# ]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input:
+# s = "pineapplepenapple"
+# wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+# Output:
+# [
+# "pine apple pen apple",
+# "pineapple pen apple",
+# "pine applepen apple"
+# ]
+# Explanation: Note that you are allowed to reuse a dictionary word.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input:
+# s = "catsandog"
+# wordDict = ["cats", "dog", "sand", "and", "cat"]
+# Output:
+# []
+# 
+#
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        return self.dfs(s, wordDict, {})
+
+    def dfs(self, s, wordDict, memo):
+        if s in memo: return memo[s]
+        if not s: return []
+
+        res = []
+        for word in wordDict:
+            if not s.startswith(word):
+                continue
+            if len(word) == len(s):
+                res.append(word)
+            else:
+                resultOfTheRest = self.dfs(s[len(word):], wordDict, memo)
+                for item in resultOfTheRest:
+                    item = word + ' ' + item
+                    res.append(item)
+        memo[s] = res
+        return res
